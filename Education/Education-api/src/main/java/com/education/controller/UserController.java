d341f83310b808b23652b076061fa2aa78e90dc2
荣波 <rongbo@rograndec.com>
2017-11-07 10:20:42 +0800

    测试分支提交代码

--- Education/Education-api/src/main/java/com/education/controller/UserController.java
+++ Education/Education-api/src/main/java/com/education/controller/UserController.java
@@ -33,6 +33,7 @@ public class UserController {
 //        User user = new User():
 //z这里是我从本地主干拉的分支  修改了代码
         //增加代码
+        //本地拉分支后的开发代码
         return new ResponseEntity<User>(user, HttpStatus.OK);
     }
 }
